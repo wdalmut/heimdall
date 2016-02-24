32cc07b5f8f48cc3edec3428116091111f3bf65f
Walter Dal Mut <walter.dalmut@gmail.com>
2016-02-24 20:42:50 +0100

    Moved to wcli in order to use subcommands

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var Table = require('cli-table');
 var i = require('./package.json');
 var fs = require('fs');
 var path = require('path');
+var Cli = require('wcli');
 
 exports.myCli = function(options) {
   return {
@@ -15,74 +16,56 @@ exports.myCli = function(options) {
       });
     },
     "run": function(args) {
-      cli.setApp("heimdall", i.version);
-      var commands = [
-        "Version: " + i.version,
-        "",
-        "Commands:",
-        "  history PATH",
-        "    Show a file history (all versions)",
-        "  revision PATH",
-        "    Get single file revision information",
-        "  bifrost PATH",
-        "    Download all revisions of a given file",
-      ];
-      cli.setUsage("heimdall COMMAND [OPTIONS] [ARGS]\n\n" + commands.join("\n"));
-
-      var opts = {
-        bucket: ['b', 'The bucket name', 'string', 'your.bucketname'],
-        region: ['r', 'Your current region', 'string', 'eu-west-1'],
-      };
-
-      switch (args[0]) {
-        case 'revision':
-          _.extend(opts,{
-            version: ["v", "The file version identification string", "string", false],
-          });
-          break;
-        case 'bifrost':
-          _.extend(opts,{
-            path: ["p", "The base folder path", "string", __dirname],
-          });
-          break;
-      }
-
-      cli.parse(opts);
-
       var that = this;
-      cli.main(function(args, options) {
-        var command = that[args[0]];
+      cli = new Cli({
+        "history": {
+          bucket: ['b', 'The bucket name', 'your.bucketname'],
+          region: ['r', 'Your current region', 'eu-west-1'],
+        },
+        "revision": {
+          bucket: ['b', 'The bucket name', 'your.bucketname'],
+          region: ['r', 'Your current region', 'eu-west-1'],
+          version: ["v", "The file version identification string", ''],
+        },
+        "bifrost": {
+          bucket: ['b', 'The bucket name', 'your.bucketname'],
+          region: ['r', 'Your current region', 'eu-west-1'],
+          path: ["p", "The base folder path", __dirname],
+        },
+      });
+      cli.app = "heimdall-s3";
+
+      cli.revision = function(options, args) {
         that.init(options);
-        return (command) ? command.apply(that, [options, args.slice(1)]) : cli.getUsage();
-      });
-    },
-    "revision": function(options, args) {
-      require('./revision')(this.s3).get(args[0], options).then(function(data) {
-        cli.output(data.Body.toString());
-      }, function(err) {
-        cli.fatal(err.toString());
-      });
-    },
-    "history": function(options, args) {
-      var that = this;
-      require('./history')(this.s3).get(args[0], options).then(function(data) {
-        var table = that.createTable(['#', 'Rev', 'Date', 'Path']);
-        _.each(data.Versions, function(elem, index) {
-          table.push([index, elem.VersionId, elem.LastModified.toString(), elem.Key]);
+        require('./revision')(that.s3).get(args[0], options).then(function(data) {
+          cli.info(data.Body.toString());
+        }, function(err) {
+          cli.fatal(err.toString());
         });
-        cli.output(table.toString());
-      }, function(err) {
-        cli.fatal(err.toString());
-      });
-    },
-    "bifrost": function(options, args) {
-      require('./bifrost')(this.s3).get(args[0], options).then(function(data) {
-        _.each(data, function(elem) {
-          cli.info("Downloaded file at: " + elem.filename);
+      };
+      cli.history = function(options, args) {
+        that.init(options);
+        require('./history')(that.s3).get(args[0], options).then(function(data) {
+          var table = that.createTable(['#', 'Rev', 'Date', 'Path']);
+          _.each(data.Versions, function(elem, index) {
+            table.push([index, elem.VersionId, elem.LastModified.toString(), elem.Key]);
+          });
+          cli.info(table.toString());
+        }, function(err) {
+          cli.fatal(err.toString());
         });
-      }, function(err) {
-        cli.fatal(err.toString());
-      });
+      };
+      cli.bifrost = function(options, args) {
+        that.init(options);
+        require('./bifrost')(that.s3).get(args[0], options).then(function(data) {
+          _.each(data, function(elem) {
+            cli.info("Downloaded file at: " + elem.filename);
+          });
+        }, function(err) {
+          cli.fatal(err.toString());
+        });
+      };
+      cli.parse(args);
     },
     "createTable": function(cols) {
       return new Table({
